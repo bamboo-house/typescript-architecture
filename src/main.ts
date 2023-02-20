082b40a93b78284f2109f845990120e8f4a0f59c
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-02-20 16:04:25 +0900

    expressセットアップ

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/api/hello", async (req, res) => {
+  res.json({
+    message: "Hello World!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Reversi application started: http://localhost:${PORT}`);
+});
